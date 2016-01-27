81b06688d05b4a6bec7d075d7c2f6a6ede018a06
Anton Arhipov <ant.arhipov@gmail.com>
2016-01-27 16:59:36 +0200

    more data

--- src/main/java/com/zt/Init.java
+++ src/main/java/com/zt/Init.java
@@ -32,13 +32,19 @@ public class Init implements ServletContextListener {
 
       System.out.println("Collections cleaned up");
 
-      supplements.insert(new BasicDBObject("name", "apples").append("price_id", "1"));
-      supplements.insert(new BasicDBObject("name", "babanas").append("price_id", "2"));
-      supplements.insert(new BasicDBObject("name", "carrots").append("price_id", "3"));
+      supplements.insert(new BasicDBObject("name", "apple").append("price_id", "1"));
+      supplements.insert(new BasicDBObject("name", "babana").append("price_id", "2"));
+      supplements.insert(new BasicDBObject("name", "carrot").append("price_id", "3"));
+      supplements.insert(new BasicDBObject("name", "potato").append("price_id", "4"));
+      supplements.insert(new BasicDBObject("name", "onion").append("price_id", "5"));
+      supplements.insert(new BasicDBObject("name", "melon").append("price_id", "6"));
 
       supplementPrices.insert(new BasicDBObject("id", "1").append("value", "123.23"));
       supplementPrices.insert(new BasicDBObject("id", "2").append("value", "234.54"));
       supplementPrices.insert(new BasicDBObject("id", "3").append("value", "345.65"));
+      supplementPrices.insert(new BasicDBObject("id", "4").append("value", "543.44"));
+      supplementPrices.insert(new BasicDBObject("id", "5").append("value", "112.11"));
+      supplementPrices.insert(new BasicDBObject("id", "6").append("value", "662.13"));
 
       System.out.println("Number of docs in supplements collection: " + supplements.getCount());
       System.out.println("Number of docs in supplementsPrices collection: " + supplementPrices.getCount());
